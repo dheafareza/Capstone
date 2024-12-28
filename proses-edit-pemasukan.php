698994c9edcdc73d304f77b5f00679ab0c4e86a1
dheafareza <dheafrz21@gmail.com>
2024-12-29 00:36:20 +0700

    First commit

---
+++ proses-edit-pemasukan.php
@@ -0,0 +1,41 @@
+<?php
+
+session_start();
+
+include('koneksi.php');
+
+ define('LOG','log.txt');
+function write_log($log){  
+ $time = @date('[Y-d-m:H:i:s]');
+ $op=$time.' '.$log."\n".PHP_EOL;
+ $fp = @fopen(LOG, 'a');
+ $write = @fwrite($fp, $op);
+ @fclose($fp);
+}
+
+$id = (int) $_GET['id_pemasukan'];
+$tgl = $_GET['tgl_pemasukan'];
+$jumlah = abs((int) $_GET['jumlah']);
+$sumber = abs((int) $_GET['id_sumber']);
+
+// Validasi format tanggal
+if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $tgl)) {
+    die("ERROR: Format tanggal tidak valid.");
+}
+
+//query update
+$query = mysqli_query($koneksi,"UPDATE pemasukan SET tgl_pemasukan='$tgl' , jumlah='$jumlah', id_sumber='$sumber' WHERE id_pemasukan='$id' ");
+
+$namaadmin = $_SESSION['nama'];
+if ($query) {
+write_log("Nama Admin : ".$namaadmin." => Edit Pemasukan => ".$id." => Sukses Edit");
+ # credirect ke page index
+ header("location:pendapatan.php"); 
+}
+else{
+write_log("Nama Admin : ".$namaadmin." => Edit Pemasukan => ".$id." => Gagal Edit");
+ echo "ERROR, data gagal diupdate". mysqli_error($koneksi);
+}
+
+//mysql_close($host);
+?>
